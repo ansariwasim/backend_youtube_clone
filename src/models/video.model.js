cdf41646479edc37f70fc5deeca5bb65969f3505
Wasim Ansari <oasimansari0786@gmail.com>
2026-09-24 17:10:24 +0545

    created video model

---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+
+    videoFile: {
+        type: String, // cloudinary url
+        required: true
+    },
+    thumbnail: {
+        type: String, // cloudinary url
+        required: true 
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    duration: {
+        type: Number,  // cloudinary 
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+
+
+}, {timestamps: true})
+
+export const Video = mongoose.model("User", videoSchema)
