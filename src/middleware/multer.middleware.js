11bb73728d510faf009197f1f457190dd39ad378
Wasim Ansari <oasimansari0786@gmail.com>
2026-09-26 19:08:13 +0545

    multer setup

---
+++ src/middleware/multer.middleware.js
@@ -0,0 +1,16 @@
+
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cd){
+    cd(null, './public/temp')
+  },
+  filename: function(req, file, cb) {
+  
+    cd(null, file.originalname)
+  }
+})
+
+export const upload = multer({
+    storage: storage,
+})
